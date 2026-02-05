4ccbcbb64d2b0f0791d0ec77a3beb6ed7229e692
Rajamohan4487 <rajamohan4487@gmail.com>
2026-02-05 22:11:01 +0530

    roboshop common functions

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -5,10 +5,10 @@ source ./common.sh
 app_name="rabbitmq"
 
 cp "$SCRIPTD"/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>"$LOGS_FILE"
-Validate $? "Copying the repos"
+VALIDATE $? "Copying the repos"
 
 dnf install rabbitmq-server -y &>>"$LOGS_FILE"
-Validate $? "installing rabbitmq"
+VALIDATE $? "installing rabbitmq"
 
 systemctl enable rabbitmq-server &>>"$LOGS_FILE"
 systemctl start rabbitmq-server &>>"$LOGS_FILE"
@@ -16,13 +16,13 @@ systemctl start rabbitmq-server &>>"$LOGS_FILE"
 rabbitmqctl list_users | grep -w "roboshop" &>>"$LOGS_FILE"
 if [ $? -ne 0 ]; then
     rabbitmqctl add_user roboshop roboshop123 &>>"$LOGS_FILE"
-    Validate $? "Creating roboshop user"
+    VALIDATE $? "Creating roboshop user"
 else
     echo -e "Roboshop user already exists ... $Y SKIPPING $N"
 fi
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>"$LOGS_FILE"
-Validate $? "setting permissions"
+VALIDATE $? "setting permissions"
 
 systemctl enable rabbitmq-server &>>"$LOGS_FILE"
 systemctl restart rabbitmq-server &>>"$LOGS_FILE"
-Validate $? "starting rabbitmq"
+VALIDATE $? "starting rabbitmq"
