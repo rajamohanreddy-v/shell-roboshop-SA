98600ede27ad722a20922f8f7744ff9acc4cf1c0
Rajamohan4487 <rajamohan4487@gmail.com>
2026-02-05 22:09:19 +0530

    roboshop common functions

--- mongodb.sh
+++ mongodb.sh
@@ -5,17 +5,17 @@ source ./common.sh
 checkroot
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>"$LOGS_FILE"
-Validate $? "Copying mongo repo"
+VALIDATE $? "Copying mongo repo"
 
 dnf install mongodb-org -y &>>"$LOGS_FILE"
-Validate $? "Installing mongodb"
+VALIDATE $? "Installing mongodb"
 
 systemctl enable mongod &>>"$LOGS_FILE"
 systemctl start mongod &>>"$LOGS_FILE"
-Validate $? "starting mongod"
+VALIDATE $? "starting mongod"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>"$LOGS_FILE"
-Validate $? "DB now heariing on internet"
+VALIDATE $? "DB now heariing on internet"
 
 systemctl restart mongod &>>"$LOGS_FILE"
-Validate $? "re-starting mongod"
+VALIDATE $? "re-starting mongod"
